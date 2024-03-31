0b07dd9a66f9ae88f42989e14ddf9d52b86bec3c
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-31 22:37:19 +0200

    update saveNewBeer, add @PostMapping

--- mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class BeerController {
         return new ResponseEntity<BeerDto>(BeerDto.builder().id(beerId).build(), HttpStatus.OK);
     }
 
+    @PostMapping("")
     public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto dto) {
         // TODO: use service
         HttpHeaders headers = new HttpHeaders();
