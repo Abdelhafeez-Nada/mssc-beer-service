5fccfb5e12cd363a6b542e9061f12ab02ebb1ad4
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-31 19:30:44 +0200

    create BeerDto

---
+++ mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,27 @@
+package org.abdelhafeez.msscbeerservice.web.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class BeerDto {
+
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quatityOnHand;
+}
