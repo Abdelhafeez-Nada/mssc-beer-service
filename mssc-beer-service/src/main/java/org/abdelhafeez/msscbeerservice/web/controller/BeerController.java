9d9fa89dee6f90727fb2ff65ad29b99632083d03
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-31 22:36:20 +0200

    implement saveNewBeer endpoint

--- mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
@@ -3,10 +3,12 @@ package org.abdelhafeez.msscbeerservice.web.controller;
 import java.util.UUID;
 
 import org.abdelhafeez.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,14 @@ public class BeerController {
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
         // TODO: use service
         return new ResponseEntity<BeerDto>(BeerDto.builder().id(beerId).build(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto dto) {
+        // TODO: use service
+        HttpHeaders headers = new HttpHeaders();
+        // TODO: get real id from saved entity & use it in Location header
+        headers.add("Location", "api/v1/beer" + UUID.randomUUID().toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
     }
 
 }
