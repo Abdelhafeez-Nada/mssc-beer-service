f9d632527b0fdcb31f55438d57bc247e5ecd4f79
Abdelhafeez <abdelhafeez0nada@gmail.com>
2024-03-31 22:55:17 +0200

    implement updateBeerById endpoint

--- mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/org/abdelhafeez/msscbeerservice/web/controller/BeerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +33,12 @@ public class BeerController {
         // TODO: get real id from saved entity & use it in Location header
         headers.add("Location", "api/v1/beer" + UUID.randomUUID().toString());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<Void> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto dto) {
+        // TODO: use service
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
